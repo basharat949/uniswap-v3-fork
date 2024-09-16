2c21e5e91a01416835acc37bd5c27a408d5702c0
basharat949 <hafiz.m.basharat@gmail.com>
2024-09-16 11:17:54 +0500

    update the priceOralce contracts

--- contracts/PriceOracle.sol
+++ contracts/PriceOracle.sol
@@ -20,6 +20,7 @@ contract PriceOracle {
         // Convert the tick to a price using the TickMath library
         uint160 sqrtPriceX96 = TickMath.getSqrtRatioAtTick(arithmeticMeanTick);
 
+
         // Calculate the price using the sqrtPriceX96 value
         price = (uint256(sqrtPriceX96) * uint256(sqrtPriceX96)) / (2 ** 192);
 
@@ -29,38 +30,41 @@ contract PriceOracle {
             _tokenIn,
             _tokenOut
         );
+
     }
 
     function getPrices(
-        address pool,
-        address token0,
-        address token1
+        address pool
+
     ) public view returns (uint256 price0, uint256 price1) {
         (uint160 sqrtPriceX96, , , , , , ) = IUniswapV3Pool(pool).slot0();
+        // uint160 sqrtPriceX96 = TickMath.getSqrtRatioAtTick(tick);
+        
+        address token0 = IUniswapV3Pool(pool).token0();
+        address token1 = IUniswapV3Pool(pool).token1();
 
         // Get the decimals for both tokens
         uint8 decimals0 = IERC20(token0).decimals();
         uint8 decimals1 = IERC20(token1).decimals();
 
-        // Price of token0 in terms of token1
-        price0 = (uint256(sqrtPriceX96) * uint256(sqrtPriceX96)) / (2 ** 192);
+        uint256 baseAmountToken0 = 10 ** decimals0;
+        uint256 baseAmountToken1 = 10 ** decimals1;
 
-        // price1 = (1 << 192) * 1e18 / (uint256(sqrtPriceX96) * uint256(sqrtPriceX96));
 
-        if (price0 != 0) {
-            if (decimals0 > decimals1) {
-                // price1 = (1 << 192) * (10 ** (decimals0 - decimals1)) / (uint256(sqrtPriceX96) * uint256(sqrtPriceX96));
-                price1 = (1e18 / price0) * (10 ** (decimals0 - decimals1));
-            }
-            if (decimals1 > decimals0) {
-                // price1 = (1 << 192) * (10 ** (decimals1 - decimals0)) / (uint256(sqrtPriceX96) * uint256(sqrtPriceX96));
-                price1 = (1e18 / price0) * (10 ** (decimals1 - decimals0));
-            }
-            if (decimals0 == decimals1) {
-                // price1 = (1 << 192) * 1e18 / (uint256(sqrtPriceX96) * uint256(sqrtPriceX96));
-                price1 = (1e18 / price0) * (10 ** (decimals1 - decimals0));
-            }
-        } else  price1 = (1 << 192) * 1e18 / (uint256(sqrtPriceX96) * uint256(sqrtPriceX96));
+        if (sqrtPriceX96 <= type(uint128).max) {
+            uint256 ratioX192 = uint256(sqrtPriceX96) * sqrtPriceX96;
+            // Price of token0 in terms of token1
+            price0 = FullMath.mulDiv(ratioX192, baseAmountToken0, 1 << 192);
+            // Price of token1 in terms of token0
+            price1 = FullMath.mulDiv(1 << 192, baseAmountToken1, ratioX192);
+        } else {
+            uint256 ratioX128 = FullMath.mulDiv(sqrtPriceX96, sqrtPriceX96, 1 << 64);
+            // Price of token0 in terms of token1
+            price0 = FullMath.mulDiv(ratioX128, baseAmountToken0, 1 << 128);
+            // Price of token1 in terms of token0
+            price1 = FullMath.mulDiv(1 << 128, baseAmountToken1, ratioX128);
+        }
 
+        price1 = decimals0 >= decimals1 ? price1 * (10 ** (decimals0 - decimals1)) : price1 * (10 ** (decimals1 - decimals0));
     }
 }
--- scripts/consult.js
+++ scripts/consult.js
@@ -8,22 +8,21 @@ async function main() {
     const PriceOracle = await ethers.getContractFactory("PriceOracle");
     const priceOracle = await PriceOracle.deploy();
 
-    console.log("PriceOracle deployed to:", priceOracle.target);
-
     // Call the consult function to get the price
     const secondsAgo = 1;
-    const amountIn = ethers.parseUnits("1");
+    const amountIn = ethers.parseUnits("1",6);
     const tokenIn = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
     const tokenOut = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
+    // const tokenOut = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'
+    // const tokenIn = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'
     const {price, quoteAmount} = await priceOracle.getAveragePrice(poolAddress, secondsAgo, amountIn,tokenIn,tokenOut);
+    
     console.log(`Price from ${secondsAgo} seconds ago:`, price.toString(), "Quote amount is:", quoteAmount.toString());
 
-    const {price0, price1} = await priceOracle.getPrices(poolAddress, tokenIn, tokenOut);
+    const {price0, price1} = await priceOracle.getPrices(poolAddress);
 
     console.log("Price0 :", price0.toString(), "Price1 is:", price1.toString());
 }
-
-
 
 main().catch((error) => {
     console.error(error);
